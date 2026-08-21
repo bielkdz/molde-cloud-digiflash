e55b617fcc08ac8588dc7870e2272ba9853c6a32
bielkdz <bielcosta3101@gmail.com>
2026-08-21 10:32:47 -0400

    Adicionar login por email e gestão de usuários

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./auth-users.css";
 createRoot(document.getElementById("root")!).render(<StrictMode><App/></StrictMode>);
